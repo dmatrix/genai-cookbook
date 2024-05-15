53c58a6a31c8740adace1d50c1512f6f6bd7180d
Jules Damji <dmatrix@comcast.net>
2024-05-14 19:39:02 -0700

    Added a Naive RAG example, added RAG module to utils file, and upated links in README
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ dspy/09_dspy_naive_rag.py
@@ -0,0 +1,40 @@
+import dspy
+import warnings
+from dspy_utils import RAG, BOLD_BEGIN, BOLD_END
+
+# Questions to ask the RAG model
+QUESTIONS = [
+    "What is the capital of Tanzania?",
+    "Who was the president of the United States in 1960?",
+    "What is the largest mammal?",
+    "What is the most populous country?",
+    "What is the most widely spoken language?",
+    "Which country won the FIFA Football World Cup in 1970?",
+    "Which country has won the most FIFA Football World Cups?",
+    "Who is the author of the book '1984'?",
+    "What is the most popular programming language?",
+    "What is dark matter in physics?",
+]
+    
+if __name__ == "__main__":
+
+    # Filter out warnings
+    warnings.filterwarnings("ignore")
+
+    # Instantiate our Language Model
+    # Setup OLlama environment on the local machine
+    ollama_mistral = dspy.OllamaLocal(model='mistral',
+                                      max_tokens=2500)
+    # Instantiate the ColBERTv2 as Retrieval module
+    colbert_rm = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')
+
+    # Configure the settings
+    dspy.settings.configure(lm=ollama_mistral, rm=colbert_rm)
+
+    # Instantiate the RAG module
+    rag = RAG(num_passages=5)
+    for idx, question in enumerate(QUESTIONS):
+        print(f"{BOLD_BEGIN}Question {idx + 1}: {BOLD_END}{question}")
+        response = rag(question=question)
+        print(f"{BOLD_BEGIN}Answer    : {BOLD_END}{response.answer}")
+        print("-----------------------------\n")
